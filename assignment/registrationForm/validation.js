096595db2f143a53101cddf076e8c7dea78f1a76
Gokul2003g <gokul2003g@gmail.com>
2022-12-18 19:03:37 +0530

    completed with form and anokha page

--- assignment/registrationForm/validation.js
+++ assignment/registrationForm/validation.js
@@ -1,7 +1,5 @@
 function formValidation()
 {
-var uid = document.registration.userid;
-var passid = document.registration.passid;
 var uname = document.registration.username;
 var uadd = document.registration.address;
 var ucountry = document.registration.country;
@@ -9,8 +7,6 @@ var uzip = document.registration.zip;
 var uemail = document.registration.email;
 var umsex = document.registration.msex;
 var ufsex = document.registration.fsex; 
-if(userid_validation(uid,5,12)) {
-  if(passid_validation(passid,7,12)) {
     if(allLetter(uname)) {
       if(alphanumeric(uadd)) { 
         if(countryselect(ucountry)) {
@@ -22,33 +18,8 @@ if(userid_validation(uid,5,12)) {
           } 
         }
       }
-    }
-  }
   return false;
 }
-
-
-function userid_validation(uid,mx,my) {
-  var uid_len = uid.value.length;
-  if (uid_len == 0 || uid_len >= my || uid_len < mx) {
-    alert("User Id should not be empty / length be between "+mx+" to "+my);
-    uid.focus();
-    return false;
-  }
-  return true;
-}
-
-
-function passid_validation(passid,mx,my) {
-  var passid_len = passid.value.length;
-if (passid_len == 0 ||passid_len >= my || passid_len < mx) {
-    alert("Password should not be empty / length be between "+mx+" to "+my);
-    passid.focus();
-    return false;
-  }
-  return true;
-}
-
 
 
 function allLetter(uname) { 
